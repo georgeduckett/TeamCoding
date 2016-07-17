7192794e38b8b2fdf1005539ec8da10944f26aff
George Duckett <georgeduckett@gmail.com>
2016-07-17 14:52:28 +0100

    Moved string extension into it's own class

---
+++ TeamCoding/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCoding.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetCorrectCaseOfParentFolder(this string fileOrFolder)
+        { // http://stackoverflow.com/a/29751774
+            string myParentFolder = Path.GetDirectoryName(fileOrFolder);
+            string myChildName = Path.GetFileName(fileOrFolder);
+            if (ReferenceEquals(myParentFolder, null))
+            {
+                return fileOrFolder.TrimEnd(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar }).ToUpperInvariant();
+            }
+
+            if (Directory.Exists(myParentFolder))
+            {
+                //myParentFolder = GetLongPathName.Invoke(myFullName);
+                string myFileOrFolder = Directory.GetFileSystemEntries(myParentFolder, myChildName).FirstOrDefault();
+                if (!ReferenceEquals(myFileOrFolder, null))
+                {
+                    myChildName = Path.GetFileName(myFileOrFolder);
+                }
+            }
+            return GetCorrectCaseOfParentFolder(myParentFolder) + Path.DirectorySeparatorChar + myChildName;
+        }
+    }
+}
