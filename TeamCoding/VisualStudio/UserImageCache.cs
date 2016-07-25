9c73834e6414e84396418aca367fa63ef2a55be8
George Duckett <georgeduckett@gmail.com>
2016-07-25 22:06:13 +0100

    Replaced placeholder image with letter with background

--- TeamCoding/VisualStudio/UserImageCache.cs
+++ TeamCoding/VisualStudio/UserImageCache.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using TeamCoding.Documents;
 using TeamCoding.IdentityManagement;
+using TeamCoding.Extensions;
 
 namespace TeamCoding.VisualStudio
 {
@@ -16,36 +17,36 @@ namespace TeamCoding.VisualStudio
     /// </summary>
     public class UserImageCache // TODO: If we go with redis as a messaging option, maybe make it also a user image caching option
     {
-        private static readonly List<System.Drawing.Brush> VisuallyDistinctColours = new List<System.Drawing.Brush>
+        private static readonly List<SolidColorBrush> VisuallyDistinctColours = new List<SolidColorBrush>
             {
-                new System.Drawing.SolidBrush(UIntToColor(0xFFFFB300)), //Vivid Yellow
-                new System.Drawing.SolidBrush(UIntToColor(0xFF803E75)), //Strong Purple
-                new System.Drawing.SolidBrush(UIntToColor(0xFFFF6800)), //Vivid Orange
-                new System.Drawing.SolidBrush(UIntToColor(0xFFA6BDD7)), //Very Light Blue
-                new System.Drawing.SolidBrush(UIntToColor(0xFFC10020)), //Vivid Red
-                new System.Drawing.SolidBrush(UIntToColor(0xFFCEA262)), //Grayish Yellow
-                new System.Drawing.SolidBrush(UIntToColor(0xFF817066)), //Medium Gray
-                new System.Drawing.SolidBrush(UIntToColor(0xFF007D34)), //Vivid Green
-                new System.Drawing.SolidBrush(UIntToColor(0xFFF6768E)), //Strong Purplish Pink
-                new System.Drawing.SolidBrush(UIntToColor(0xFF00538A)), //Strong Blue
-                new System.Drawing.SolidBrush(UIntToColor(0xFFFF7A5C)), //Strong Yellowish Pink
-                new System.Drawing.SolidBrush(UIntToColor(0xFF53377A)), //Strong Violet
-                new System.Drawing.SolidBrush(UIntToColor(0xFFFF8E00)), //Vivid Orange Yellow
-                new System.Drawing.SolidBrush(UIntToColor(0xFFB32851)), //Strong Purplish Red
-                new System.Drawing.SolidBrush(UIntToColor(0xFFF4C800)), //Vivid Greenish Yellow
-                new System.Drawing.SolidBrush(UIntToColor(0xFF7F180D)), //Strong Reddish Brown
-                new System.Drawing.SolidBrush(UIntToColor(0xFF93AA00)), //Vivid Yellowish Green
-                new System.Drawing.SolidBrush(UIntToColor(0xFF593315)), //Deep Yellowish Brown
-                new System.Drawing.SolidBrush(UIntToColor(0xFFF13A13)), //Vivid Reddish Orange
-                new System.Drawing.SolidBrush(UIntToColor(0xFF232C16)), //Dark Olive Green
+                new SolidColorBrush(UIntToColor(0xFFFFB300)), //Vivid Yellow
+                new SolidColorBrush(UIntToColor(0xFF803E75)), //Strong Purple
+                new SolidColorBrush(UIntToColor(0xFFFF6800)), //Vivid Orange
+                new SolidColorBrush(UIntToColor(0xFFA6BDD7)), //Very Light Blue
+                new SolidColorBrush(UIntToColor(0xFFC10020)), //Vivid Red
+                new SolidColorBrush(UIntToColor(0xFFCEA262)), //Grayish Yellow
+                new SolidColorBrush(UIntToColor(0xFF817066)), //Medium Gray
+                new SolidColorBrush(UIntToColor(0xFF007D34)), //Vivid Green
+                new SolidColorBrush(UIntToColor(0xFFF6768E)), //Strong Purplish Pink
+                new SolidColorBrush(UIntToColor(0xFF00538A)), //Strong Blue
+                new SolidColorBrush(UIntToColor(0xFFFF7A5C)), //Strong Yellowish Pink
+                new SolidColorBrush(UIntToColor(0xFF53377A)), //Strong Violet
+                new SolidColorBrush(UIntToColor(0xFFFF8E00)), //Vivid Orange Yellow
+                new SolidColorBrush(UIntToColor(0xFFB32851)), //Strong Purplish Red
+                new SolidColorBrush(UIntToColor(0xFFF4C800)), //Vivid Greenish Yellow
+                new SolidColorBrush(UIntToColor(0xFF7F180D)), //Strong Reddish Brown
+                new SolidColorBrush(UIntToColor(0xFF93AA00)), //Vivid Yellowish Green
+                new SolidColorBrush(UIntToColor(0xFF593315)), //Deep Yellowish Brown
+                new SolidColorBrush(UIntToColor(0xFFF13A13)), //Vivid Reddish Orange
+                new SolidColorBrush(UIntToColor(0xFF232C16)), //Dark Olive Green
             };
-        static public System.Drawing.Color UIntToColor(uint color)
+        static public Color UIntToColor(uint color)
         {
             var a = (byte)(color >> 24);
             var r = (byte)(color >> 16);
             var g = (byte)(color >> 8);
             var b = (byte)(color >> 0);
-            return System.Drawing.Color.FromArgb(a, r, g, b);
+            return Color.FromArgb(a, r, g, b);
         }
         private static readonly Brush BorderBrush = new SolidColorBrush(new Color() { ScA = 0.65f, ScR = 1.0f, ScG = 1.0f, ScB = 1.0f });
         //private readonly BitmapImage SharedUnknownUserImage = LoadBitmapFromResource("Resources/UnknownUserImage.png");
@@ -56,70 +57,64 @@ namespace TeamCoding.VisualStudio
         {
             IdeWrapper = ideWrapper;
         }
-        private BitmapImage ConvertToBitmapImage(System.Drawing.Bitmap src)
-        { // http://stackoverflow.com/a/34590774
-            using (var ms = new MemoryStream())
+        private static Panel CreateUserIdentityControl(UserIdentity userIdentity)
+        {
+            var firstLetter = userIdentity.Id[0];
+            var grid = new Grid()
             {
-                src.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                BitmapImage image = new BitmapImage();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.BeginInit();
-                ms.Position = 0;
-                image.StreamSource = ms;
-                image.EndInit();
-                return image;
+                Background = VisuallyDistinctColours[firstLetter % VisuallyDistinctColours.Count]
+            };
+
+            grid.Children.Add(new Viewbox()
+            { // TODO: use http://stackoverflow.com/questions/9785322/textblock-as-big-as-a-capital-letter-ignoring-font-ascender-descender#15876463 to centre lower-case letters properly
+                // TODO: figure out why background changes when changing name
+                // TODO: when clearing name (or adding in?) make sure image can be replaced with control and visa-versa
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Stretch = Stretch.Uniform,
+                StretchDirection = StretchDirection.DownOnly,
+                Child = new TextBlock()
+                {
+                    LineHeight = 8f,
+                    LineStackingStrategy = LineStackingStrategy.MaxHeight,
+                    Text = firstLetter.ToString(),
+                    TextAlignment = TextAlignment.Center,
+                    TextTrimming = TextTrimming.None,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                }
+            });
+            grid.Children.Add(new Border()
+            {
+                BorderBrush = BorderBrush,
+                BorderThickness = new Thickness(1),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch
+            });
+
+            return grid;
+        }
+        private static void UpdateIdentityControl(Panel parentControl, SourceControlledDocumentData matchedRemoteDoc)
+        {
+            var contentControl = parentControl.FindChild<ContentControl>();
+
+            if (contentControl != null)
+            {
+                contentControl.Content = (matchedRemoteDoc.IdeUserIdentity.DisplayName ?? matchedRemoteDoc.IdeUserIdentity.Id)[0];
             }
         }
-        public System.Drawing.Font GetAdjustedFont(System.Drawing.Graphics GraphicRef, string GraphicString, System.Drawing.Font OriginalFont, int ContainerSize, int MaxFontSize, int MinFontSize, bool SmallestOnFail)
+        private static void UpdateUserImageControl(Panel parentControl, SourceControlledDocumentData matchedRemoteDoc)
         {
-            System.Drawing.Font testFont = null;
-            // We utilize MeasureString which we get via a control instance           
-            for (int AdjustedSize = MaxFontSize; AdjustedSize >= MinFontSize; AdjustedSize--)
+            parentControl.ToolTip = (matchedRemoteDoc.IdeUserIdentity.DisplayName ?? matchedRemoteDoc.IdeUserIdentity.Id) + (matchedRemoteDoc.BeingEdited ? " [edited]" : string.Empty);
+
+            if (matchedRemoteDoc.HasFocus)
             {
-                testFont = new System.Drawing.Font(OriginalFont.Name, AdjustedSize, OriginalFont.Style);
-
-                // Test the string with the new size
-                System.Drawing.SizeF AdjustedSizeNew = GraphicRef.MeasureString(GraphicString, testFont);
-
-                if (ContainerSize > Convert.ToInt32(AdjustedSizeNew.Width) && ContainerSize > Convert.ToInt32(AdjustedSizeNew.Height))
-                {
-                    // Good font, return it
-                    return testFont;
-                }
-            }
-
-            // If you get here there was no fontsize that worked
-            // return MinimumSize or Original?
-            if (SmallestOnFail)
-            {
-                return testFont;
+                parentControl.Children.OfType<Border>().Single().Visibility = System.Windows.Visibility.Visible;
             }
             else
             {
-                return OriginalFont;
+                parentControl.Children.OfType<Border>().Single().Visibility = System.Windows.Visibility.Hidden;
             }
-        }
-        private ImageSource CreatePlaceholderImage(UserIdentity identity, int desiredSize)
-        {
-            if (UrlImages.ContainsKey(identity.DisplayName))
-            {
-                return UrlImages[identity.DisplayName];
-            }
-
-            var result = new System.Drawing.Bitmap(desiredSize, desiredSize);
-            using (var g = System.Drawing.Graphics.FromImage(result))
-            {
-                var firstLetter = (identity.DisplayName?[0] ?? identity?.Id?[0]).Value;
-
-                g.FillRectangle(VisuallyDistinctColours[firstLetter % VisuallyDistinctColours.Count], new System.Drawing.Rectangle(0, 0, result.Width, result.Height));
-                var FontSize = GetAdjustedFont(g, firstLetter.ToString(), new System.Drawing.Font("Courier New", 18f), result.Width, 18, 1, true);
-                
-                // Draw the first letter on the user image
-                var letterSize = g.MeasureString(firstLetter.ToString(), FontSize);
-                g.DrawString(firstLetter.ToString(), FontSize, System.Drawing.Brushes.White, (result.Width - letterSize.Width) / 2, (result.Height - letterSize.Height) / 2);
-            }
-            // TODO: Why is this image blank? - Maybe we should instead create a drawing element that renders the background colour and text automatically
-            return UrlImages[identity.DisplayName] = ConvertToBitmapImage(result);
         }
         private static Panel CreateUserImageControl(ImageSource imageSource)
         {
@@ -165,11 +160,11 @@ namespace TeamCoding.VisualStudio
                     return CreateUserImageControl(UrlImages[userIdentity.ImageUrl]);
                 }
 
-                result = CreateUserImageControl(CreatePlaceholderImage(userIdentity, desiredSize));
+                result = CreateUserIdentityControl(userIdentity);
             }
             else
             {
-                return CreateUserImageControl(CreatePlaceholderImage(userIdentity, desiredSize));
+                return CreateUserIdentityControl(userIdentity);
             }
 
             if (userIdentity.ImageUrl != null)
@@ -205,18 +200,8 @@ namespace TeamCoding.VisualStudio
         }
         internal void SetImageProperties(Panel parentControl, SourceControlledDocumentData matchedRemoteDoc)
         {
-            var imageControl = parentControl as Panel;
-
-            imageControl.ToolTip = (matchedRemoteDoc.IdeUserIdentity.DisplayName ?? matchedRemoteDoc.IdeUserIdentity.Id) + (matchedRemoteDoc.BeingEdited ? " [edited]" : string.Empty);
-
-            if (matchedRemoteDoc.HasFocus)
-            {
-                imageControl.Children.OfType<Border>().Single().Visibility = System.Windows.Visibility.Visible;
-            }
-            else
-            {
-                imageControl.Children.OfType<Border>().Single().Visibility = System.Windows.Visibility.Hidden;
-            }
+            UpdateIdentityControl(parentControl, matchedRemoteDoc);
+            UpdateUserImageControl(parentControl, matchedRemoteDoc);
         }
     }
 }
