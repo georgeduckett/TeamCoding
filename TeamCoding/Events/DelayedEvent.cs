580f09a4cf7fa189f5431a612c4ae2529be26f07
George Duckett <georgeduckett@gmail.com>
2016-08-20 21:26:16 +0100

    Move DelayedEvent to it's own class

---
+++ TeamCoding/Events/DelayedEvent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TeamCoding.Events
+{
+    public class DelayedEvent<TEventArgs> where TEventArgs : EventArgs
+    {
+        private readonly Timer Timer;
+        private TEventArgs LatestEventArgs;
+        private object LatestSender;
+        private readonly object EventArgsLock = new object();
+        public event EventHandler<TEventArgs> Event;
+        public DelayedEvent(int millisecondsDelay)
+        {
+            Timer = new Timer(millisecondsDelay) { Enabled = false, AutoReset = false };
+            Timer.Elapsed += Timer_Elapsed;
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            lock (EventArgsLock)
+            {
+                Event?.Invoke(LatestSender, LatestEventArgs);
+            };
+        }
+        public void EventHandler(object sender, TEventArgs e)
+        {
+            Timer.Stop();
+            lock (EventArgsLock)
+            {
+                LatestSender = sender;
+                LatestEventArgs = e;
+            }
+            Timer.Start();
+        }
+    }
+}
--- TeamCoding/VisualStudio/TeamCodingTextViewConnectionListener.cs
+++ TeamCoding/VisualStudio/TeamCodingTextViewConnectionListener.cs
@@ -9,6 +9,7 @@ using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
 using System.Timers;
+using TeamCoding.Events;
 using TeamCoding.Extensions;
 
 namespace TeamCoding.VisualStudio
@@ -20,6 +21,7 @@ namespace TeamCoding.VisualStudio
     {
         private readonly ITextDocumentFactoryService TextDocFactory;
         private readonly IClassifierAggregatorService TextClassifierService;
+        private readonly DelayedEvent<CaretPositionChangedEventArgs> DelayedCaretPositionChanged = new DelayedEvent<CaretPositionChangedEventArgs>(500);
 
         [ImportingConstructor]
         public TeamCodingTextViewConnectionListener(ITextDocumentFactoryService textDocumentFactoryService, IClassifierAggregatorService textClassifierService)
@@ -27,7 +29,11 @@ namespace TeamCoding.VisualStudio
             TextDocFactory = textDocumentFactoryService;
             TextClassifierService = textClassifierService;
             TextDocFactory.TextDocumentDisposed += TextDocFactory_TextDocumentDisposed;
-            Timer.Elapsed += Timer_Elapsed;
+            DelayedCaretPositionChanged.Event += DelayedCaretPositionChanged_Event;
+        }
+        private async void DelayedCaretPositionChanged_Event(object sender, CaretPositionChangedEventArgs e)
+        {
+            await TeamCodingPackage.Current.LocalIdeModel.OnCaretPositionChanged(e);
         }
         public void SubjectBuffersConnected(IWpfTextView textView, ConnectionReason reason, Collection<ITextBuffer> subjectBuffers)
         {
@@ -41,35 +47,14 @@ namespace TeamCoding.VisualStudio
                 if (textDoc != null)
                 {
                     textDoc.FileActionOccurred += TextDoc_FileActionOccurred;
-                    textView.Caret.PositionChanged += Caret_PositionChanged;
+                    textView.Caret.PositionChanged += DelayedCaretPositionChanged.EventHandler;
                 }
             }
         }
-
-        private readonly Timer Timer = new Timer(500) { Enabled = false, AutoReset = false };
-        private CaretPositionChangedEventArgs CaretPositionChangedArgs;
-        private void Caret_PositionChanged(object sender, CaretPositionChangedEventArgs e)
-        {
-            Timer.Stop();
-            CaretPositionChangedArgs = e;
-            Timer.Start();
-        }
-
-        private async void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            CaretPositionChangedEventArgs localCaretPositionChangedArgs;
-            lock (CaretPositionChangedArgs)
-            {
-                localCaretPositionChangedArgs = CaretPositionChangedArgs;
-            };
-            await TeamCodingPackage.Current.LocalIdeModel.OnCaretPositionChanged(CaretPositionChangedArgs);
-        }
-
         private void TextDocFactory_TextDocumentDisposed(object sender, TextDocumentEventArgs e)
         {
             TeamCodingPackage.Current.LocalIdeModel.OnTextDocumentDisposed(e.TextDocument, e);
         }
-
         private void TextDoc_FileActionOccurred(object sender, TextDocumentFileActionEventArgs e)
         {
             if (e.FileActionType == FileActionTypes.ContentSavedToDisk || e.FileActionType == FileActionTypes.DocumentRenamed)
@@ -83,12 +68,10 @@ namespace TeamCoding.VisualStudio
                 }
             }
         }
-
         private void TextBuffer_Changed(object sender, TextContentChangedEventArgs e)
         {
             TeamCodingPackage.Current.LocalIdeModel.OnTextBufferChanged(sender as ITextBuffer, e);
         }
-
         public void SubjectBuffersDisconnected(IWpfTextView textView, ConnectionReason reason, Collection<ITextBuffer> subjectBuffers)
         {
             if (reason == ConnectionReason.TextViewLifetime)
@@ -100,7 +83,7 @@ namespace TeamCoding.VisualStudio
                 if (textDoc != null)
                 {
                     textDoc.FileActionOccurred -= TextDoc_FileActionOccurred;
-                    textView.Caret.PositionChanged -= Caret_PositionChanged;
+                    textView.Caret.PositionChanged -= DelayedCaretPositionChanged.EventHandler;
                 }
             }
         }
