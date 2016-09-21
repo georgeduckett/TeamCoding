22e9b828a1401e961d3b4107e4a4a110464490af
George Duckett <georgeduckett@gmail.com>
2016-09-21 20:14:19 +0100

    Added empty service project

---
+++ TeamCoding.WindowsService/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCoding.WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase.Run(new[] { new TeamCodingSyncServer() });
+        }
+    }
+}
---
+++ TeamCoding.WindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TeamCoding.WindowsService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TeamCodingServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.TeamCodingServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // TeamCodingServiceProcessInstaller
+            // 
+            this.TeamCodingServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.TeamCodingServiceProcessInstaller.Password = null;
+            this.TeamCodingServiceProcessInstaller.Username = null;
+            // 
+            // TeamCodingServiceInstaller
+            // 
+            this.TeamCodingServiceInstaller.Description = "Server for syncing Team Coding Visual Studio Extensions users.";
+            this.TeamCodingServiceInstaller.DisplayName = "TeamCoding Sync Server";
+            this.TeamCodingServiceInstaller.ServiceName = "TeamCoding Sync";
+            this.TeamCodingServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.TeamCodingServiceProcessInstaller,
+            this.TeamCodingServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller TeamCodingServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller TeamCodingServiceInstaller;
+    }
+}
