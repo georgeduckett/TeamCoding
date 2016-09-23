5e1951d9d194957d3cb23c61e25314427e4c294b
George Duckett <georgeduckett@gmail.com>
2016-09-23 22:51:25 +0100

    Don't refresh data points that don't need it

--- TeamCoding.v14/VisualStudio/CodeLens/CurrentUsersDataPointUpdater.cs
+++ TeamCoding.v14/VisualStudio/CodeLens/CurrentUsersDataPointUpdater.cs
@@ -18,6 +18,7 @@ namespace TeamCoding.VisualStudio.CodeLens
         private readonly IRemoteModelPersister RemoteModelChangeManager = TeamCodingProjectTypeProvider.Get<ITeamCodingPackageProvider>().RemoteModelChangeManager;
         private readonly List<CurrentUsersDataPointViewModel> DataPointModels = new List<CurrentUsersDataPointViewModel>();
         private Dictionary<int[], string> CaretMemberHashCodeToDataPointString = new Dictionary<int[], string>(new IntArrayEqualityComparer());
+        private Dictionary<ICodeElementDescriptor, string> CodeElementDescriptorToDataPointString = new Dictionary<ICodeElementDescriptor, string>();
         private bool disposedValue = false; // To detect redundant calls
         public CurrentUsersDataPointUpdater(): base()
         {
@@ -42,17 +43,35 @@ namespace TeamCoding.VisualStudio.CodeLens
                                                   CaretMemberHashCodes = of.CaretPositionInfo.SyntaxNodeIds,
                                                   of.IdeUserIdentity.DisplayName
                                               })
-                                              .GroupBy(of => of.CaretMemberHashCodes, new IntArrayEqualityComparer())
-                                              .ToDictionary(g => g.Key, g => "Current coders: " + string.Join(", ", g.Select(of => of.DisplayName).Distinct()));
+                                              .GroupBy(of => of.CaretMemberHashCodes,
+                                                       new IntArrayEqualityComparer())
+                                              .ToDictionary(g => g.Key,
+                                                            g => "Current coders: " + string.Join(", ", g.Select(of => of.DisplayName).Distinct()),
+                                                            new IntArrayEqualityComparer());
 
             if (!oldCaretMemberHashCodeToDataPointString.DictionaryEqual(CaretMemberHashCodeToDataPointString))
             {
                 foreach (var dataPointModel in DataPointModels)
                 {
-                    if (!dataPointModel.IsDisposed)
+                    var codeElementDescriptor = ((CurrentUsersDataPoint)dataPointModel.DataPoint).CodeElementDescriptor;
+                    if (dataPointModel.IsDisposed)
                     {
-                        if (dataPointModel.RefreshCommand.CanExecute(null))
+                        CodeElementDescriptorToDataPointString.Remove(codeElementDescriptor);
+                    }
+                    else
+                    {
+                        bool shouldRefresh = false;
+                        var newText = GetTextForDataPointInternal(codeElementDescriptor);
+
+                        if (dataPointModel.Descriptor != newText ||
+                            !dataPointModel.IsVisible && !string.IsNullOrEmpty(newText))
                         {
+                            shouldRefresh = true;
+                        }
+
+                        if (shouldRefresh && dataPointModel.RefreshCommand.CanExecute(null))
+                        {
+                            CodeElementDescriptorToDataPointString[codeElementDescriptor] = newText;
                             dataPointModel.RefreshCommand.Execute(null);
                         }
                     }
@@ -60,7 +79,7 @@ namespace TeamCoding.VisualStudio.CodeLens
                 DataPointModels.RemoveAll(dvm => dvm.IsDisposed);
             }
         }
-        public Task<string> GetTextForDataPoint(ICodeElementDescriptor codeElementDescriptor)
+        private string GetTextForDataPointInternal(ICodeElementDescriptor codeElementDescriptor)
         {
             foreach (var caret in CaretMemberHashCodeToDataPointString.Keys)
             {
@@ -69,7 +88,7 @@ namespace TeamCoding.VisualStudio.CodeLens
                 // Find the first node that we start the node chain from (any node that is tracked; a class or member declaration etc)
                 var syntaxNodeChain = node.AncestorsAndSelf().ToArray();
                 var trackedLeafNodes = syntaxNodeChain.Where(n => n.IsTrackedLeafNode()).Reverse().ToArray();
-                
+
                 var foundMatch = false;
                 for (int i = 0; i < trackedLeafNodes.Length; i++)
                 {
@@ -81,7 +100,7 @@ namespace TeamCoding.VisualStudio.CodeLens
                         foundMatch = false;
                         continue;
                     }
-                    
+
                     // Now walk up the tree from the matching one, and up the method hashes ensuring we match all the way up
                     var nodeancestorhashes = matchedLeafNode.AncestorsAndSelf().Select(a => a.GetValueBasedHashCode());
                     if (nodeancestorhashes.SequenceEqual(caret.Take(caretMatchedHashIndex + 1).Reverse()))
@@ -96,10 +115,23 @@ namespace TeamCoding.VisualStudio.CodeLens
 
                 if (foundMatch)
                 {
-                    return Task.FromResult(CaretMemberHashCodeToDataPointString[caret]);
+                    return CaretMemberHashCodeToDataPointString[caret];
                 }
             }
-            return Task.FromResult<string>(null);
+            return null;
+        }
+        public Task<string> GetTextForDataPoint(ICodeElementDescriptor codeElementDescriptor)
+        {
+            if (CodeElementDescriptorToDataPointString.ContainsKey(codeElementDescriptor))
+            {
+                return Task.FromResult(CodeElementDescriptorToDataPointString[codeElementDescriptor]);
+            }
+            else
+            {
+                var result = GetTextForDataPointInternal(codeElementDescriptor);
+                CodeElementDescriptorToDataPointString.Add(codeElementDescriptor, result);
+                return Task.FromResult(result);
+            }
         }
         protected virtual void Dispose(bool disposing)
         {
