a5c92a2dfe9bb62a131c99466a694be5b9558d9e
George Duckett <georgeduckett@gmail.com>
2016-09-27 15:03:20 +0100

    Remove redundent comment

--- TeamCoding/Extensions/DocumentViewExtensions.cs
+++ TeamCoding/Extensions/DocumentViewExtensions.cs
@@ -6,7 +6,6 @@ namespace TeamCoding.Extensions
     {
         public static string GetRelatedFilePath(this DocumentView documentView)
         {
-            // TODO: Is there a better way to get the tab's full file path than parsing the documentview's name?
             var fileName = documentView.Name.Split('|')[2];
             return fileName.GetCorrectCaseOfParentFolder();
         }
