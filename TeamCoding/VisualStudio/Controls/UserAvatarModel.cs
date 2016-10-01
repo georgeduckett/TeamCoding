005742ed224cfb15d1b20166184f2b8452255920
George Duckett <georgeduckett@gmail.com>
2016-10-01 08:53:28 +0100

    Added property updated notify events

--- TeamCoding/VisualStudio/Controls/UserAvatarModel.cs
+++ TeamCoding/VisualStudio/Controls/UserAvatarModel.cs
@@ -11,15 +11,91 @@ namespace TeamCoding.VisualStudio.Controls
 {
     public class UserAvatarModel : INotifyPropertyChanged
     {
-        public ImageSource AvatarImageSource { get; set; }
-        public Brush BorderBrush { get; set; }
-        public Visibility BorderVisibility { get; set; }
-        public string ToolTip { get; set; }
-        public Brush BackgroundBrush { get; set; }
-        public char? Letter { get; set; }
-        public Brush LetterBrush { get; set; }
-        public Thickness LetterMargin { get; set; }
+        private ImageSource _AvatarImageSource;
+        public ImageSource AvatarImageSource
+        {
+            get { return _AvatarImageSource; }
+            set
+            {
+                _AvatarImageSource = value;
+                OnPropertyChanged(nameof(AvatarImageSource));
+            }
+        }
+        private Brush _BorderBrush;
+        public Brush BorderBrush
+        {
+            get { return _BorderBrush; }
+            set
+            {
+                _BorderBrush = value;
+                OnPropertyChanged(nameof(BorderBrush));
+            }
+        }
+        private Visibility _BorderVisibility;
+        public Visibility BorderVisibility
+        {
+            get { return _BorderVisibility; }
+            set
+            {
+                _BorderVisibility = value;
+                OnPropertyChanged(nameof(BorderVisibility));
+            }
+        }
+        private string _ToolTip;
+        public string ToolTip
+        {
+            get { return _ToolTip; }
+            set
+            {
+                _ToolTip = value;
+                OnPropertyChanged(nameof(ToolTip));
+            }
+        }
+        private Brush _BackgroundBrush;
+        public Brush BackgroundBrush
+        {
+            get { return _BackgroundBrush; }
+            set
+            {
+                _BackgroundBrush = value;
+                OnPropertyChanged(nameof(BackgroundBrush));
+            }
+        }
+        private char? _Letter;
+        public char? Letter
+        {
+            get { return _Letter; }
+            set
+            {
+                _Letter = value;
+                OnPropertyChanged(nameof(Letter));
+            }
+        }
+        private Brush _LetterBrush;
+        public Brush LetterBrush
+        {
+            get { return _LetterBrush; }
+            set
+            {
+                _LetterBrush = value;
+                OnPropertyChanged(nameof(LetterBrush));
+            }
+        }
+        private Thickness _LetterMargin;
+        public Thickness LetterMargin
+        {
+            get { return _LetterMargin; }
+            set
+            {
+                _LetterMargin = value;
+                OnPropertyChanged(nameof(LetterMargin));
+            }
+        }
 
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
