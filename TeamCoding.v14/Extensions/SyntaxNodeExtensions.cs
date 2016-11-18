e3a6d7b59fd4b04b10c081fa650ed1d6ba55f127
George Duckett <georgeduckett@gmail.com>
2016-11-18 11:25:47 +0000

    Fixed getting hashcodes of syntax nodes

--- TeamCoding.v14/Extensions/SyntaxNodeExtensions.cs
+++ TeamCoding.v14/Extensions/SyntaxNodeExtensions.cs
@@ -23,12 +23,112 @@ namespace TeamCoding.Extensions
                    syntaxNode is TypeBlockSyntax ||
                    syntaxNode is MethodBlockBaseSyntax;
         }
-        public static int GetNameBasedHashCode(this SyntaxNode syntaxNode)
+        private static string GetParameterTypesString(this Microsoft.CodeAnalysis.VisualBasic.Syntax.ParameterListSyntax typeParameters)
         {
-            // TODO: Get name from Microsoft.TeamFoundation.CodeSense.Client.Common.dll
-            // Get name based on all types the classes CSharpSyntaxNodeExtensions.PropertyVisitor and VisualBasicSyntaxNodeExtensions.PropertyVisitor visit
-            // If language is Visual Basic use StringComparer.OrdinalIgnoreCase.GetHashCode(name) else StringComparer.Ordinal(name)
-            return 0;
+            if (typeParameters?.Parameters.Any() ?? false)
+            {
+                return "(" + string.Join(", ", typeParameters?.Parameters.Select(p => p.AsClause?.Type?.ToString() ?? "Nothing")) + ")";
+            }
+            return string.Empty;
+        }
+        private static string GetParameterTypesString(this Microsoft.CodeAnalysis.CSharp.Syntax.ParameterListSyntax typeParameters)
+        {
+            if (typeParameters?.Parameters.Any() ?? false)
+            {
+                return "(" + string.Join(", ", typeParameters?.Parameters.Select(p => p.Type.ToString())) + ")";
+            }
+            return string.Empty;
+        }
+        private static string GetGenericParametersString(this Microsoft.CodeAnalysis.VisualBasic.Syntax.TypeParameterListSyntax typeParameters)
+        {
+            if (typeParameters?.Parameters.Any() ?? false)
+            {
+                return "<" + typeParameters?.Parameters.Count().ToString() + ">";
+            }
+            return string.Empty;
+        }
+        private static string GetGenericParametersString(this Microsoft.CodeAnalysis.CSharp.Syntax.TypeParameterListSyntax typeParameters)
+        {
+            if (typeParameters?.Parameters.Any() ?? false)
+            {
+                return "<" + typeParameters?.Parameters.Count().ToString() + ">";
+            }
+            return string.Empty;
+        }
+        public static string GetName(this SyntaxNode syntaxNode)
+        {
+            string name = null;
+            switch (syntaxNode)
+            {
+                // Don't throw an exception on null, just return null
+                case null: return null;
+                // If we're a visual basic node and the parent is a "block" type one then we should return null as it's the parent that has the name;
+                case VisualBasicSyntaxNode typedNode when (typedNode.Parent.GetType().Name.Contains("Block")): return null;
+                // CSharp Nodes
+                case ConstructorDeclarationSyntax typedNode: name = typedNode.Identifier.ToString(); break;
+                case DestructorDeclarationSyntax typedNode: name = "~" + typedNode.Identifier.ToString(); break;
+
+                case ClassDeclarationSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case StructDeclarationSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case DelegateDeclarationSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString() + typedNode.ParameterList.GetParameterTypesString(); break;
+                case InterfaceDeclarationSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+
+                case NamespaceDeclarationSyntax typedNode: name = typedNode.Name.ToString(); break;
+                case EnumDeclarationSyntax typedNode: name = typedNode.Identifier.ToString(); break;
+                case OperatorDeclarationSyntax typedNode: name = typedNode.OperatorToken.ToString(); break;
+                case ConversionOperatorDeclarationSyntax typedNode: name = typedNode.Type.ToString(); break;
+                    // TODO: Handle multiple indexers with different types
+                case IndexerDeclarationSyntax typedNode: name = typedNode.ExplicitInterfaceSpecifier != null ? typedNode.ExplicitInterfaceSpecifier.Name + ".[]" : "[]"; break;
+                
+                case MethodDeclarationSyntax typedNode:
+                    name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString() + typedNode.ParameterList.GetParameterTypesString();
+                    if (typedNode.ExplicitInterfaceSpecifier != null)
+                    {
+                        name = typedNode.ExplicitInterfaceSpecifier.Name.ToString() + "." + name;
+                    }
+                    break;
+                case PropertyDeclarationSyntax typedNode:
+                    name = typedNode.Identifier.ToString();
+                    if (typedNode.ExplicitInterfaceSpecifier != null)
+                    {
+                        name = typedNode.ExplicitInterfaceSpecifier.Name.ToString() + "." + name;
+                    }
+                    break;
+
+                // Visual Basic Nodes
+                case ClassStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case DelegateStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString() + typedNode.ParameterList.GetParameterTypesString(); break;
+                case InterfaceStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case MethodStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString() + typedNode.ParameterList.GetParameterTypesString(); break;
+                case ModuleStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case StructureStatementSyntax typedNode: name = typedNode.Identifier.ToString() + typedNode.TypeParameterList.GetGenericParametersString(); break;
+                case DeclareStatementSyntax typedNode: name = typedNode.Identifier.ToString(); break;
+                case EnumStatementSyntax typedNode: name = typedNode.Identifier.ToString(); break;
+                case NamespaceStatementSyntax typedNode: name = typedNode.Name.ToString(); break;
+                case OperatorStatementSyntax typedNode: name = typedNode.OperatorToken.ToString(); break;
+                case PropertyStatementSyntax typedNode: name = typedNode.Identifier.ToString(); break;
+                case SubNewStatementSyntax typedNode: name = typedNode.NewKeyword.ToString(); break;
+                case ClassBlockSyntax typedNode: name = typedNode.ClassStatement.GetName(); break;
+                case ConstructorBlockSyntax typedNode: name = typedNode.BlockStatement.GetName(); break;
+                case EnumBlockSyntax typedNode: name = typedNode.EnumStatement.GetName(); break;
+                case InterfaceBlockSyntax typedNode: name = typedNode.BlockStatement.GetName(); break;
+                case MethodBlockSyntax typedNode: name = typedNode.BlockStatement.GetName(); break;
+                case ModuleBlockSyntax typedNode: name = typedNode.BlockStatement.GetName(); break;
+                case NamespaceBlockSyntax typedNode: name = typedNode.NamespaceStatement.GetName(); break;
+                case PropertyBlockSyntax typedNode: name = typedNode.PropertyStatement.GetName(); break;
+                case StructureBlockSyntax typedNode: name = typedNode.BlockStatement.GetName(); break;
+                default: return syntaxNode.Parent.GetName();
+            }
+
+            string parentName = syntaxNode.Parent.GetName();
+            if (parentName != null)
+            {
+                return parentName + "." + name;
+            }
+            else
+            {
+                return name;
+            }
         }
         public static int GetValueBasedHashCode(this SyntaxNode syntaxNode)
         {
@@ -42,7 +142,13 @@ namespace TeamCoding.Extensions
                 return _SyntaxNodeHashes[syntaxNode];
             }
 
-            var identityHash = syntaxNode.GetNameBasedHashCode();
+            var name = syntaxNode.GetName();
+
+            var identityHash = 0;
+
+            if (name != null) {
+                identityHash = syntaxNode is VisualBasicSyntaxNode ? StringComparer.OrdinalIgnoreCase.GetHashCode(name) : StringComparer.Ordinal.GetHashCode(name);
+            }
 
             var fieldDeclarationNodeCS = syntaxNode as Microsoft.CodeAnalysis.CSharp.Syntax.FieldDeclarationSyntax;
             var fieldDeclarationNodeVB = syntaxNode as Microsoft.CodeAnalysis.VisualBasic.Syntax.FieldDeclarationSyntax;
