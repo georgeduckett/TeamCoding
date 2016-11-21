43e59532c224cf1290d11f561281759abcca2106
George Duckett <georgeduckett@gmail.com>
2016-11-21 15:55:08 +0000

    Converted RedisWrapper to wait for the reset event as a task.

---
+++ TeamCoding/Extensions/WaitHandleExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TeamCoding.Extensions
+{
+    public static class WaitHandleExtensions
+    { // http://stackoverflow.com/a/18766131
+        public static Task<bool> AsTask(this WaitHandle handle) => AsTask(handle, Timeout.InfiniteTimeSpan);
+        public static Task<bool> AsTask(this WaitHandle handle, TimeSpan timeout)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            var registration = ThreadPool.RegisterWaitForSingleObject(handle, (state, timedOut) =>
+            {
+                var localTcs = (TaskCompletionSource<bool>)state;
+                localTcs.TrySetResult(!timedOut);
+            }, tcs, timeout, executeOnlyOnce: true);
+            tcs.Task.ContinueWith((_, state) => ((RegisteredWaitHandle)state).Unregister(null), registration, TaskScheduler.Default);
+            return tcs.Task;
+        }
+    }
+}
