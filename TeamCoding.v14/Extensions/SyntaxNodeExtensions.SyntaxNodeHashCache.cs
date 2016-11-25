9bcb00a456db62d7d31895f786f9a74cbcb78ec5
George Duckett <georgeduckett@gmail.com>
2016-11-25 23:11:43 +0000

    Moved SyntaxNodeHashCache to its own file

---
+++ TeamCoding.v14/Extensions/SyntaxNodeExtensions.SyntaxNodeHashCache.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCoding.Extensions
+{
+    public static partial class SyntaxNodeExtensions
+    {
+        private static class SyntaxNodeHashCache
+        {
+            private static readonly Dictionary<SyntaxNode, int> _SyntaxNodeHashes = new Dictionary<SyntaxNode, int>();
+            private static readonly MultiValueDictionary<string, SyntaxNode> _SyntaxNodesFromFilePath = new MultiValueDictionary<string, SyntaxNode>();
+
+            public static bool TryGetHash(SyntaxNode node, out int hash) => _SyntaxNodeHashes.TryGetValue(node, out hash);
+
+            public static void Add(SyntaxNode syntaxNode, int identityHash)
+            {
+                var filePath = syntaxNode.SyntaxTree?.FilePath;
+                if (syntaxNode is ICompilationUnitSyntax && filePath != null && _SyntaxNodesFromFilePath.ContainsKey(filePath))
+                {
+                    // If we've got a new compilation unit syntax then any syntax nodes from the same file won't get used so remove them.
+                    foreach (var node in _SyntaxNodesFromFilePath[filePath])
+                    {
+                        _SyntaxNodeHashes.Remove(node);
+                    }
+                    _SyntaxNodesFromFilePath.Remove(filePath);
+                }
+
+                if (filePath != null)
+                {
+                    _SyntaxNodesFromFilePath.Add(filePath, syntaxNode);
+                }
+                _SyntaxNodeHashes.Add(syntaxNode, identityHash);
+            }
+        }
+    }
+}
--- TeamCoding.v14/Extensions/SyntaxNodeExtensions.cs
+++ TeamCoding.v14/Extensions/SyntaxNodeExtensions.cs
@@ -12,37 +12,8 @@ using System.Threading.Tasks;
 
 namespace TeamCoding.Extensions
 {
-    public static class SyntaxNodeExtensions
+    public static partial class SyntaxNodeExtensions
     {
-        private static class SyntaxNodeHashCache
-        {
-            private static readonly Dictionary<SyntaxNode, int> _SyntaxNodeHashes = new Dictionary<SyntaxNode, int>();
-            private static readonly MultiValueDictionary<string, SyntaxNode> _SyntaxNodesFromFilePath = new MultiValueDictionary<string, SyntaxNode>();
-
-            public static bool TryGetHash(SyntaxNode node, out int hash) => _SyntaxNodeHashes.TryGetValue(node, out hash);
-
-            public static void Add(SyntaxNode syntaxNode, int identityHash)
-            {
-                var filePath = syntaxNode.SyntaxTree?.FilePath;
-                if (syntaxNode is ICompilationUnitSyntax && filePath != null && _SyntaxNodesFromFilePath.ContainsKey(filePath))
-                {
-                    // If we've got a new compilation unit syntax then any syntax nodes from the same file won't get used so remove them.
-                    foreach (var node in _SyntaxNodesFromFilePath[filePath])
-                    {
-                        _SyntaxNodeHashes.Remove(node);
-                    }
-                    _SyntaxNodesFromFilePath.Remove(filePath);
-                }
-
-                if (filePath != null)
-                {
-                    _SyntaxNodesFromFilePath.Add(filePath, syntaxNode);
-                }
-                _SyntaxNodeHashes.Add(syntaxNode, identityHash);
-            }
-        }
-
-
         public static bool IsTrackedLeafNode(this SyntaxNode syntaxNode)
         {
             return syntaxNode is MemberDeclarationSyntax ||
