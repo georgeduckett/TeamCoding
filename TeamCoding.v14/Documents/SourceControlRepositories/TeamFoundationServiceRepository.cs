962037fffa7105ea51ee1f4f50dc520678bbbab3
George Duckett <georgeduckett@gmail.com>
2017-03-12 22:42:42 +0000

    Removed temp code for TFS diffs; a server query each time is no good.

--- TeamCoding.v14/Documents/SourceControlRepositories/TeamFoundationServiceRepository.cs
+++ TeamCoding.v14/Documents/SourceControlRepositories/TeamFoundationServiceRepository.cs
@@ -15,57 +15,7 @@ namespace TeamCoding.Documents.SourceControlRepositories
     {
         public (int[] LineAdditions, int[] LineDeletions)? GetDiffWithServer(string fullFilePath)
         {
-            return null;
-
-            /*var workspaceInfo = Workstation.Current.GetLocalWorkspaceInfo(fullFilePath);
-            if (workspaceInfo == null) return null;
-
-            var projectCollection = new TfsTeamProjectCollection(workspaceInfo.ServerUri);
-            if (projectCollection == null) return null;
-
-            var versionControlServer = projectCollection.GetService<VersionControlServer>();
-            if (versionControlServer == null) return null;
-            projectCollection.Credentials = System.Net.CredentialCache.DefaultCredentials;
-            try
-            {
-                projectCollection.EnsureAuthenticated();
-            }
-            catch (TeamFoundationServerUnauthorizedException ex)
-            {
-                TeamCodingProjectTypeProvider.Get<ITeamCodingPackageProvider>().Logger.WriteError(ex);
-                return null;
-            }
-
-            if (!versionControlServer.ServerItemExists(fullFilePath, ItemType.File)) return null;
-
-            var serverPath = workspaceInfo.GetWorkspace(projectCollection).GetServerItemForLocalItem(fullFilePath);
-            var serverVersion = new DiffItemVersionedFile(versionControlServer, serverPath, VersionSpec.Latest);
-            var localVersion = new DiffItemLocalFile(fullFilePath, Encoding.UTF8.CodePage, DateTime.Now, false);
-
-            using (var stream = new MemoryStream())
-            using (var writer = new StreamWriter(stream))
-            {
-                var diffOptions = new DiffOptions
-                {
-                    Flags = DiffOptionFlags.EnablePreambleHandling,
-                    OutputType = DiffOutputType.Unified,
-                    TargetEncoding = Encoding.UTF8,
-                    SourceEncoding = Encoding.UTF8,
-                    StreamWriter = writer
-                };
-
-                Difference.DiffFiles(versionControlServer, serverVersion, localVersion, diffOptions, serverPath, true);
-                writer.Flush();
-                stream.Position = 0;
-
-                using (var reader = new StreamReader(stream))
-                {
-                    var line = reader.ReadLine();
-                }
-            }
-
-            return null; // TODO: Get the proper diff for TFS repositories
-            */
+            return null; // TODO: Get the proper diff for TFS repositories (because TFS Diff would require querying the server we need to maintain Diff ourselves)
         }
 
         public DocumentRepoMetaData GetRepoDocInfo(string fullFilePath)
