ebc09adf6bb770a8e4b21e1cfaa52f791f2e6ad0
George Duckett <georgeduckett@gmail.com>
2017-03-18 11:18:23 +0000

    Fix null reference when working on a local-only branch

--- TeamCoding/Documents/SourceControlRepositories/GitRepository.cs
+++ TeamCoding/Documents/SourceControlRepositories/GitRepository.cs
@@ -33,18 +33,18 @@ namespace TeamCoding.Documents.SourceControlRepositories
             if (repo.Ignore.IsPathIgnored(relativePath)) return null;
 
             var repoHeadTree = repo.Head.Tip.Tree;
-            var remoteMasterTree = repo.Head.TrackedBranch.Tip.Tree;
+            var remoteMasterTree = repo.Head.TrackedBranch?.Tip?.Tree;
 
             // Check for local changes, then server changes.
             // It's possible there is a local change that actually makes it the same as the remote, but I think that's ok to say the user is editing anyway
             var isEdited = repo.Diff.Compare<TreeChanges>(new[] { fullFilePath }).Any() ||
-                           repo.Diff.Compare<TreeChanges>(remoteMasterTree, repoHeadTree, new[] { fullFilePath }).Any();
+                           (remoteMasterTree != null && repo.Diff.Compare<TreeChanges>(remoteMasterTree, repoHeadTree, new[] { fullFilePath }).Any());
 
             return new DocumentRepoMetaData()
             {
                 RepoProvider = nameof(GitRepository),
-                RepoUrl = repo.Head.TrackedBranch.Remote.Url,
-                RepoBranch = repo.Head.TrackedBranch.CanonicalName,
+                RepoUrl = repo.Head.TrackedBranch?.Remote?.Url ?? repo.Branches.Single(b => b.IsTracking).TrackedBranch.Remote.Url,
+                RepoBranch = repo.Head.TrackedBranch?.CanonicalName ?? repo.Head.CanonicalName,
                 RelativePath = relativePath,
                 BeingEdited = isEdited,
                 LastActioned = DateTime.UtcNow
