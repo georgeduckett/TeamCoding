f2c6a286716f9ad1abe6f29ea18036a602f5845d
Matthew Steeples <matthew@mercuryit.co.uk>
2018-01-04 14:48:05 +0000

    Only update UI if there were any relevant changes

--- TeamCoding/VisualStudio/ToolWindows/OverviewWindow/OverviewControl.xaml.cs
+++ TeamCoding/VisualStudio/ToolWindows/OverviewWindow/OverviewControl.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using TeamCoding.Documents;
 using TeamCoding.Extensions;
 
 namespace TeamCoding.VisualStudio.ToolWindows.OverviewWindow
@@ -15,6 +16,9 @@ namespace TeamCoding.VisualStudio.ToolWindows.OverviewWindow
     public partial class OverviewControl : UserControl
     {
         private HashSet<string> ExpandedItems = new HashSet<string>();
+
+        private HashSet<IRemotelyAccessedDocumentData> Documents;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="OverviewControl"/> class.
         /// </summary>
@@ -27,22 +31,29 @@ namespace TeamCoding.VisualStudio.ToolWindows.OverviewWindow
 
         private void RemoteModelChangeManager_RemoteModelReceived(object sender, EventArgs e)
         {
-            tvUserDocs.DataContext = (from of in TeamCodingPackage.Current.RemoteModelChangeManager.GetOpenFiles()
-                                      group of by of.IdeUserIdentity into ofg
-                                      select new
-                                      {
-                                          Identity = ofg.Key,
-                                          UserAvatarModel = TeamCodingPackage.Current.UserImages.CreateUserAvatarModel(ofg.Key),
-                                          Documents = ofg.ToArray()
-                                      }).ToArray();
+            var newDocuments = new HashSet<IRemotelyAccessedDocumentData>(TeamCodingPackage.Current.RemoteModelChangeManager.GetOpenFiles(), OverviewWindowEqualityComparer.Instance);
 
-            var treeItems = tvUserDocs.FindChildren<TreeViewItem>();
-
-            foreach (var node in treeItems)
+            if (Documents == null || !(Documents.IsSubsetOf(newDocuments) && Documents.IsSupersetOf(newDocuments)))
             {
-                if (ExpandedItems.Contains((string)node.Tag))
+                Documents = newDocuments;
+
+                tvUserDocs.DataContext = (from of in Documents
+                                          group of by of.IdeUserIdentity into ofg
+                                          select new
+                                          {
+                                              Identity = ofg.Key,
+                                              UserAvatarModel = TeamCodingPackage.Current.UserImages.CreateUserAvatarModel(ofg.Key),
+                                              Documents = ofg.ToArray()
+                                          }).ToArray();
+
+                var treeItems = tvUserDocs.FindChildren<TreeViewItem>();
+
+                foreach (var node in treeItems)
                 {
-                    node.IsExpanded = true;
+                    if (ExpandedItems.Contains((string)node.Tag))
+                    {
+                        node.IsExpanded = true;
+                    }
                 }
             }
         }
@@ -55,6 +66,40 @@ namespace TeamCoding.VisualStudio.ToolWindows.OverviewWindow
         private void TreeViewItem_Collapsed(object sender, RoutedEventArgs e)
         {
             ExpandedItems.Remove((string)((TreeViewItem)sender).Tag);
+        }
+
+        class OverviewWindowEqualityComparer : IEqualityComparer<IRemotelyAccessedDocumentData>
+        {
+            public static OverviewWindowEqualityComparer Instance { get; } = new OverviewWindowEqualityComparer();
+
+            public bool Equals(IRemotelyAccessedDocumentData x, IRemotelyAccessedDocumentData y)
+            {
+                if (x == null ^ y == null)
+                    return false;
+
+                return x.Repository == y.Repository &&
+                   x.RepositoryBranch == y.RepositoryBranch &&
+                   x.RelativePath == y.RelativePath &&
+                   x.IdeUserIdentity.Id == y.IdeUserIdentity.Id &&
+                   x.BeingEdited == y.BeingEdited &&
+                   x.HasFocus == y.HasFocus
+                   ;
+            }
+
+            public int GetHashCode(IRemotelyAccessedDocumentData obj)
+            {
+                var hash = 17;
+                hash = hash * 31 + obj.Repository.GetHashCode();
+
+                if (!string.IsNullOrEmpty(obj.RepositoryBranch))
+                    hash = hash * 31 + obj.RepositoryBranch.GetHashCode();
+
+                hash = hash * 31 + obj.IdeUserIdentity.Id.GetHashCode();
+                hash = hash * 31 + obj.BeingEdited.GetHashCode();
+                hash = hash * 31 + obj.HasFocus.GetHashCode();
+
+                return hash;
+            }
         }
     }
 }
